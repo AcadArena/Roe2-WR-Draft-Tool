ec9f60cb712a5c88067977c2e2db85e9afe0d507
Jasper <jasper@manok.dev>
2022-10-21 09:35:26 +0800

    fix: fix prod_url for getImagePath

--- lib/utils/getImagePath.ts
+++ lib/utils/getImagePath.ts
@@ -1,6 +1,6 @@
 export const getImagePath = (path: string = "") => {
   const isProd = import.meta.env.VITE_VERCEL_ENV === "production";
   const DEV_URL = `http://${window.location.hostname}:1338/img?path=${path}`;
-  const PROD_URL = `https://servers.acadarena.com/img?path=${path}`;
+  const PROD_URL = `https://servers.acadarena.com/wr/img?path=${path}`;
   return isProd ? PROD_URL : DEV_URL;
 };
