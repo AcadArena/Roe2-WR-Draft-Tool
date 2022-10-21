41896af3c758441f0bcf042c8860a8289e9294f7
Jasper <jasper@manok.dev>
2022-10-21 09:03:28 +0800

    fix: use env port for port

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { createServer } from "http";
 import { initExpressServer } from "./express.server";
 import { initAssets } from "./lib/initAssets";
@@ -9,16 +10,17 @@ const app = initExpressServer();
 const server = createServer(app);
 const io = initWsServer(server);
 export const tickManager = new TickManager(io);
+const port = process.env.PORT || 1338;
 
-const port = 1338;
-
-initAssets().then(() => {
+async function main() {
+  server.listen(port, () => {
+    if (server.address() === null) {
+      return console.log("Failed to start server.");
+    }
+    console.log(`Server started on port ${port}`);
+  });
+  await initAssets();
   io.emit("champs", sanitizedChampList());
-});
+}
 
-server.listen(port, () => {
-  if (server.address() === null) {
-    return console.log("Failed to start server.");
-  }
-  console.log(`Server started on port ${port}`);
-});
+main();
